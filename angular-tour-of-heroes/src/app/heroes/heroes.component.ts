0870824e82a92c1de7e1a8a305e3ef562bab6b16
EmadRyanA <emad.ryan.a@gmail.com>
2019-06-26 14:54:10 -0400

    Setup for online lists

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
-import {HEROES} from '../mock-heroes'
+import {HeroService} from '../hero.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,13 +8,18 @@ import {HEROES} from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero
-  heroes=HEROES;
-  constructor() {   }
+  heroes:Hero[];
+  constructor(private heroService: HeroService) {   }
+
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
